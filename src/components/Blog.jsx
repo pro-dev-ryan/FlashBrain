7440b99cf13afd3257acf3862f4e37775e298cf1
pro-dev-ryan <azizul.developer@gmail.com>
2022-10-12 22:38:04 +0600

    blog added

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,8 +2,50 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <h1>This is Blog</h1>
+    <div className="flex flex-col justify-center items-center space-y-5">
+      <h1 className="text-center text-xl lg:text-2xl lg:my-5 text-white font-heading font-bold glass w-fit bg-zinc-700 bg-opacity-30 p-2">
+        This is Blog
+      </h1>
+      <div className="glass bg-white bg-opacity-60 lg:max-w-[40%] lg:m-auto lg:p-5 p-2 block text-left rounded ">
+        <h3 className="font-heading text-base lg:text-lg lg:font-semibold">
+          <span className="text-rose-400"> Que: </span>
+          what is the purpose of react router ?
+        </h3>
+        <p className="bg-zinc-700 bg-opacity-40 p-2 rounded-md">
+          <span className="text-stone-100">Ans:</span> React Router helps the
+          developer to set multiple route of a webpage. Specially This
+          functionality gave advantage in single page react Application.{" "}
+        </p>
+      </div>
+      <div className="glass bg-white bg-opacity-60 lg:max-w-[40%] lg:m-auto lg:p-5 p-2 block text-left rounded">
+        <h3 className="font-heading text-base lg:text-lg lg:font-semibold">
+          <span className="text-rose-400"> Que: </span>
+          how does context api work ?
+        </h3>
+        <p className="bg-zinc-700 bg-opacity-40 p-2 rounded-md">
+          <span className="text-stone-100">Ans:</span>
+          Context Api is another hook which gave a power to developers that they
+          can transmit data nearly anywhere in the application, with props
+          drilling. generally it should be create in the root of the
+          application. It has a provider wrapper. Data will be available to the
+          routes of those are contained by the provider wrapper. after that we
+          have to call the context by use context hook. and the data transmitted
+          through the apis are will be ready to use.{" "}
+        </p>
+      </div>
+      <div className="glass bg-white bg-opacity-60 lg:max-w-[40%] lg:m-auto lg:p-5 p-2 block text-left rounded mb-5">
+        <h3 className="font-heading text-base lg:text-lg lg:font-semibold">
+          <span className="text-rose-400"> Que: </span>
+          what is use href hook in react router ?
+        </h3>
+        <p className="bg-zinc-700 bg-opacity-40 p-2 rounded-md">
+          <span className="text-stone-100">Ans:</span>
+          The useRef is a hook that allows us directly create a reference to the
+          DOM element in the functional component. This hook will work with this
+          Syntax: const refContainer = useRef(initialValue) .The useRef returns
+          a mutable reffered object.{" "}
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,9 +1,46 @@
 import React from "react";
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
 
 const Statistics = () => {
   return (
-    <div>
-      <h2>This is stats</h2>
+    <div className="flex flex-col justify-center items-center space-y-5">
+      <h2 className="text-center text-xl lg:text-2xl lg:my-5 text-white font-heading font-bold glass w-fit bg-zinc-700 bg-opacity-30 p-2">
+        This is Statistics
+      </h2>
+      {/* <div>
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Area
+              type="monotone"
+              dataKey="uv"
+              stroke="#8884d8"
+              fill="#8884d8"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div> */}
     </div>
   );
 };
